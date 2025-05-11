0d642e1b11a419c88b85cc7b1966c435050dd4cf
Luffy <luffy07.2802@gmail.com>
2025-05-11 12:08:42 +0000

    fffmm

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2 group" aria-label="DOT007 Home">
       <Image 
-        src="/src/images/logo.jpg" // Changed from /src/images/logo.jpg
+        src="/logo.jpg" 
         alt="DOT007 Logo" 
         width={32} 
         height={32} 
